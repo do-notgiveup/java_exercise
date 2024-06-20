7f43522f5883468b3b98c12b4520935cb399a2b2
Nghĩa Phạm (Darius Pham) <nghia01694049627@gmail.com>
2024-06-20 16:53:45 +0700

    refactor b1

--- Ex 20_6/Java_String/src/Main.java
+++ Ex 20_6/Java_String/src/Main.java
@@ -1,5 +1,7 @@
 import java.lang.reflect.Array;
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.HashSet;
 import java.util.Scanner;
 
 public class Main {
@@ -8,7 +10,7 @@ public class Main {
         System.out.print("The given string is: ");
 
         System.out.println("The first non repeated character in String is: "
-                + getCharNonRepeat(sc.nextLine()));
+                + firstNonRepeatCharacterByLoop(sc.nextLine()));
 
         String[] strArray = {"Red", "Green", "Orange", "White", "Black"};
         System.out.print("List before sort: ");
@@ -19,22 +21,53 @@ public class Main {
 
         System.out.print("List after sort: ");
         String[] sortString = sortString(strArray);
+
         for (int i = 0; i <= sortString.length - 1; i++) {
             System.out.print(sortString[i] + " ");
         }
     }
 
-    public static char getCharNonRepeat(String str) {
-        char getChar = str.charAt(0);
-        for (int i = 1; i <= str.length() - 1; i++) {
-            if (i != str.length() - 1 && str.charAt(i) != getChar
-                    && str.charAt(i + 1) != str.charAt(i)) {
-                getChar = str.charAt(i);
-            } else if (i == str.length() - 1 && str.charAt(i) != getChar) {
-                getChar = str.charAt(i);
-            } else break;
+//    public static char getCharNonRepeat(String str) {
+////        char getChar = str.charAt(0);
+////        for (int i = 1; i <= str.length() - 1; i++) {
+////            if (i != str.length() - 1 && str.charAt(i) != getChar
+////                    && str.charAt(i + 1) != str.charAt(i)) {
+////                getChar = str.charAt(i);
+////            } else if (i == str.length() - 1 && str.charAt(i) != getChar) {
+////                getChar = str.charAt(i);
+////            } else break;
+////        }
+////        return getChar;
+//        char c = 0;
+//        for (int i = 0; i < str.length(); i++) {
+//            c = str.charAt(i);
+//            for (int j = 0; j < str.length(); j++) {
+//                if (i != j && str.charAt((j)) == c)
+//
+//            }
+//        }
+//        return c;
+//    }
+
+    public static String firstNonRepeatCharacterByLoop(String input){
+        // for 1: duyệt ký tự đầu tiên đến ký tự cuối của String.
+        for(int i = 0; i < input.length(); i++){
+            // đặt biến cờ là true
+            boolean flag = true;
+            // for 2: duyệt ký tự tiếp theo của input[i] đến cuối chuỗi
+            for(int j = i + 1; j < input.length(); j++){
+                // so sánh 2 ký tự liền kề nhau, nếu giống nhau thì có trùng lặp va thoát khỏi vòng lặp này
+                // và tiếp tục duyệt ký tự tiếp theo.
+                if(input.charAt(i) == input.charAt(j)){
+                    flag = false;
+                    break;
+                }
+            }
+            // nếu biến flag là true tức là ko có ký tự trùng lặp thì lập tức trả về
+            if(flag) return String.valueOf(input.charAt(i));
         }
-        return getChar;
+        // ngược lại ko có ký tự mà ko có sự trung lặp thì trả về null.
+        return null;
     }
 
     public static String[] sortString(String[] str) {
