36a9cea4fdffd6f1ac7e6aee4c2c9189faa81f35
NghiaPham <nghia01694049627@gmail.com>
2024-09-06 15:50:55 +0700

    add demo log4j

--- Ex 4_9/JWT_authentication/JWT_authentication/src/main/java/vn/edu/likelion/basic_authentication/BasicAuthenticationApplication.java
+++ Ex 4_9/JWT_authentication/JWT_authentication/src/main/java/vn/edu/likelion/basic_authentication/BasicAuthenticationApplication.java
@@ -1,13 +1,17 @@
 package vn.edu.likelion.basic_authentication;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class BasicAuthenticationApplication {
+    private static final Logger logger = LogManager.getLogger(BasicAuthenticationApplication.class);
 
-	public static void main(String[] args) {
-		SpringApplication.run(BasicAuthenticationApplication.class, args);
-	}
+    public static void main(String[] args){
+        logger.info("This is an info message"); logger.error("This is an error message");
+        SpringApplication.run(BasicAuthenticationApplication.class, args);
+    }
 
 }
--- Ex 4_9/JWT_authentication/JWT_authentication/src/main/java/vn/edu/likelion/basic_authentication/controllers/UserEntityController.java
+++ Ex 4_9/JWT_authentication/JWT_authentication/src/main/java/vn/edu/likelion/basic_authentication/controllers/UserEntityController.java
@@ -1,5 +1,6 @@
 package vn.edu.likelion.basic_authentication.controllers;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
